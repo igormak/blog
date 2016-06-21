7c69dd0225b0d52b62e409625d79ff27f88e8c7e
igormak <igor05011993@rambler.ru>
2016-06-21 12:00:08 +0300

    ruby

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,18 @@
+class CommentsController < ApplicationController
+	def create
+    	@micropost = Micropost.find(params[:micropost_id])
+    	@comment = @micropost.comments.create(comment_params )
+    	redirect_to micropost_path(@micropost)
+	end
+ 
+	def destroy
+	   @micropost = Micropost.find(params[:micropost_id])
+	   @comment = @micropost.comments.find(params[:id])
+	   @comment.destroy
+	   redirect_to micropost_path(@micropost)
+	end
+  	private
+    	def comment_params 
+      		params.require(:comment).permit(:commenter, :body)
+    	end
+end
